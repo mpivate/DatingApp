e50e7fbd1cb42bd6ca1daf60fca973062f161ba0
mpivate <mprivate8898@gmail.com>
2023-03-16 01:44:18 +0700

    End of section 8

--- API/Startup.cs
+++ API/Startup.cs
@@ -39,6 +39,10 @@ namespace API
         {
             services.AddApplicationServices(_config);
             services.AddControllers();
+            services.AddControllersWithViews()
+            .AddNewtonsoftJson(options =>
+            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+);
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
